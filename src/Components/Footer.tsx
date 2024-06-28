ed9bf22847a213cfa0646299eb7da63862dc8a15
Thomaz Alves <thomazhilario5@gmail.com>
2024-06-28 13:05:55 -0300

    feat: Home.tsx
    - Criar as Seções de categorias da página Home.

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,7 +3,7 @@ import AluraFlix_Logo from '../assets/logo-aluraflix.svg'
 
 export const Footer = () => {
     return(
-        <footer className='bg-black/90 flex justify-center items-center h-[10vh]'>
+        <footer className='bg-[#101a2e] flex justify-center items-center h-[10vh]'>
             <img src={AluraFlix_Logo} alt='Logo da AluraFlix'/>
         </footer>
     )
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,69 @@
+// React
+import { useState, useEffect } from "react"
+
 // Components
 import { Banner } from "../Components/Banner"
+import { SectionCategories } from "../Components/Section-Categories"
+
+// interface
+interface CategoryProps{
+    name:string,
+    color:string
+}
 
 export const Home = () => {
+
+    // Fazer uma requisicao quando o componente renderizar
+    useEffect(() => {
+        async function loadPosts(){
+            try {
+                const response = await fetch('http://localhost:3000/videos')
+
+                // transformando os dados em javascript
+                const data = await response.json()
+
+                setPosts(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        loadPosts()
+    },[])
+
+    // state - posts
+    const [posts, setPosts] = useState([])
+
+    // state - categorias
+    const [categorias, setCategorias] = useState<CategoryProps[]>([
+        {
+            name:'Front-End',
+            color:'#6BD1FF'
+        },
+        {
+            name:'Back-End',
+            color:'#00C86F'
+        },
+        {
+            name:'Mobile',
+            color:'#FFBA05'
+        }
+    ])
+
     return (
         <main className="bg-[#001233]">
             <Banner/>
+
+            <div className="flex flex-col gap-36 mt-10 px-[4.30rem] py-10 bg-[#191919]">
+                    {categorias.map((categoria, index) => (
+                        <SectionCategories 
+                            key={index} 
+                            name={categoria.name} 
+                            color={categoria.color}
+                            posts={posts.filter(post => post.categoria.toLowerCase() === categoria.name.toLowerCase())}
+                        />
+                    ))}
+            </div>
         </main>
     )
 }
