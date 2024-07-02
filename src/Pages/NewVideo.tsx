d1e988f74ff5ea201f1cdc379295b61f54d2ae08
Thomaz Alves <thomazhilario5@gmail.com>
2024-07-02 13:39:33 -0300

    feat: NewVideo.tsx
    - Criar o formulario ara a inserção do card.
    - Usar o react-hook-form ara validar os formulários.
    - Criando estilos condicionais.

--- src/Pages/NewVideo.tsx
+++ src/Pages/NewVideo.tsx
@@ -1,6 +1,45 @@
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+// interface
+interface SchemaProps{
+    titulo:string,
+    categoria:string,
+    imagem:string,
+    video:string,
+    descricao:string | null
+}
+
+// Schema
+const schema = z.object({
+    titulo:z.string().min(1,'Preencha o campo acima'),
+    categoria:z.string().min(1,'Preencha o campo acima'),
+    imagem:z.string().min(1,'Preencha o campo acima'),
+    video:z.string().min(1,'Preencha o campo acima'),
+    descricao:z.string()
+}).required({
+    titulo:true,
+    categoria:true,
+    imagem:true,
+    video:true,
+    descricao:true
+})
+
 export const NewVideo = () => {
+
+    const { register, handleSubmit, formState:{errors} } = useForm<SchemaProps>({resolver:zodResolver(schema)})
+
+    // tailwindCss for inputsForms
+    const inputsForms = 'flex flex-col gap-3'
+    const inputStyle = 'bg-transparent border-[#262626] border-2 pl-5 py-3 rounded-md outline-none'
+    const inputStyleErrors = 'border-red-500'
+    function handleSubmitVideoInDbJson(data:SchemaProps){
+
+    }
+
     return(
-        <main className="h-[80vh] bg-[#191919] text-white flex flex-col items-center gap-10">
+        <main className="h-screen bg-[#191919] text-white flex flex-col items-center gap-10">
             <article className="text-center">
                 <h1 className="text-[4rem] font-bold">novo vídeo</h1>
                 <p className="text-lg">Complete o formulário para criar um novo card de vídeo.</p>
@@ -8,8 +47,61 @@ export const NewVideo = () => {
 
             <section className="w-[80vw]">
                 <h2 className="text-4xl w-full border-y-4 border-[#262626] py-3">Criar Card</h2>
-                <form>
+                <form className='mt-5 grid grid-cols-2 gap-5' onSubmit={handleSubmit(handleSubmitVideoInDbJson)}>
+                    <p className={inputsForms}>
+                        <label className={`text-2xl ${errors.titulo && 'text-red-500'}`}>Titulo</label>
+                        <input 
+                            className={`${inputStyle} ${errors.titulo && inputStyleErrors}`} 
+                            type='text' 
+                            {...register('titulo')}
+                            placeholder='Digite o titulo...'
+                        />
+                    </p>
 
+                    <p className={inputsForms}>
+                        <label className={`text-2xl ${errors.categoria && 'text-red-500'}`}>Categoria</label>
+                        <select className={`${inputStyle} ${errors.categoria && inputStyleErrors}`} {...register('categoria')}>
+                            <option className='bg-[#191919]' value="Front-End">Front-end</option>
+                            <option className='bg-[#191919]' value="Back-End">Back-end</option>
+                            <option className='bg-[#191919]' value="Mobile">Mobile</option>
+                        </select>
+                    </p>
+
+                    <p className={inputsForms}>
+                        <label className={`text-2xl ${errors.imagem && 'text-red-500'}`}>imagem</label>
+                        <input 
+                            className={`${inputStyle} ${errors.imagem && inputStyleErrors}`} 
+                            type='text' 
+                            {...register('imagem')}
+                            placeholder='Digite a url da imagem...'
+                        />
+                    </p>
+
+                    <p className={inputsForms}>
+                        <label className={`text-2xl ${errors.video && 'text-red-500'}`}>Video</label>
+                        <input 
+                            className={`${inputStyle} ${errors.video && inputStyleErrors}`} 
+                            type='text' 
+                            {...register('video')}
+                            placeholder='Digite a url do video'
+                        />
+                    </p>
+
+                    <p className={inputsForms}>
+                        <label className={`text-2xl ${errors.descricao && 'text-red-500'}`}>Descricao</label>
+                        <textarea 
+                            className={`${inputStyle} ${errors.descricao && inputStyleErrors}`} 
+                            {...register('descricao')}
+                            placeholder='Digite uma descricao do video (opcional)'
+                        />
+                    </p>
+                    
+                    <br />
+
+                    <p className='flex gap-5'>
+                        <button className='text-[#2271D1] border-2 border-[#2271D1] px-7 py-2 rounded-md' type='submit'>Guardar</button>
+                        <button className='border-2 px-7 py-2 rounded-md' type='reset'>Limpar</button>
+                    </p>
                 </form>
             </section>
         </main>
