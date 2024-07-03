6fcb3a3bcb3ada143083bf70d1ac0200dd1f6132
Thomaz Alves <thomazhilario5@gmail.com>
2024-07-03 11:43:34 -0300

    style: Card.tsx
    - Adicionando icons aos buttons.
    - Adicionando props aos icons do button.
    - Alterando espaçamentos internos no container dos buttons.

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -4,14 +4,24 @@ interface CardProps{
     color:string
 }
 
+// Lucide-React
+import { PencilLine, Trash2 } from 'lucide-react'
+
 export const Card = ({imagem, color}:CardProps) => {
+
+    // iconProps
+    const iconProps = {
+        color:'white',
+        size:25
+    }
+
     return(
         <article className="h-full w-[332px] overflow-hidden rounded-lg" style={{border: `4px solid ${color}`}}>
             <img className="w-full object-cover" src={imagem} alt="imagem do video"/>
 
-            <section className="bg-black text-white flex justify-between rounded-b-lg p-2 px-14 ">
-                <button>Deletar</button>
-                <button>Editar</button>
+            <section className="bg-black text-white flex justify-between rounded-b-lg p-2 px-8 ">
+                <button className='flex items-center gap-3 text-xl'><PencilLine {...iconProps}/> Deletar</button>
+                <button className='flex items-center gap-3 text-xl'><Trash2 {...iconProps}/> Editar</button>
             </section>
         </article>
     )
