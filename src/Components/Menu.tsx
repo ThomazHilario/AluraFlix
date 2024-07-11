edf9180f6a5950f54fe136db54c6892b6fae8d6a
Thomaz Alves <thomazhilario5@gmail.com>
2024-07-11 11:40:29 -0300

    fix: Menu.tsx
    - Corrigir error de acessibilidade no componente de Menu.

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,10 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog'
+import { VisuallyHidden } from '@radix-ui/themes'
 
 // Components
 import { ButtonLink } from './Button-Link'
 
 // lucide-react
 import { MenuIcon } from 'lucide-react'
+
 
 export const Menu = () => {
     return(
@@ -14,6 +16,13 @@ export const Menu = () => {
             </Dialog.Trigger>
             <Dialog.Portal>
                 <Dialog.Content className='absolute top-14 right-1 bg-zinc-800 px-2 py-5 rounded-md'>
+                    <VisuallyHidden>
+                        <Dialog.Title>Menu de navegação</Dialog.Title>
+                        <Dialog.Description>
+                            Menu de navegação para navegar entre os links disponiveis.
+                            Home: é a página inicial, e NewVideo: pagina para adicionar um novo video no AluraFlix
+                        </Dialog.Description>
+                    </VisuallyHidden>
                     <nav className='flex flex-col gap-4 items-center'>
                         <ButtonLink name='Home' path='/'/>
                         <ButtonLink name='Novo Video' path='/newvideo'/>
